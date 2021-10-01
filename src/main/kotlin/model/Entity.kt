62198c33a8579741981fb6da39123daa43abd30a
DVT <DVT12@protonmail.com>
2021-10-01 18:32:12 -0400

    Added health property to entities

--- src/main/kotlin/model/Entity.kt
+++ src/main/kotlin/model/Entity.kt
@@ -1,3 +1,9 @@
 package model
 
-open class Entity(val name: String, val entityType: EntityType)
+open class Entity(val name: String, val entityType: EntityType, var health: Int) {
+    val isAlive: Boolean
+        get() = health > 0
+
+    val isDead: Boolean
+        get() = !isAlive
+}
--- src/main/kotlin/model/entities/Player.kt
+++ src/main/kotlin/model/entities/Player.kt
@@ -3,4 +3,4 @@ package model.entities
 import model.Entity
 import model.EntityType
 
-class Player(name: String): Entity(name, EntityType.Player)
+class Player(name: String): Entity(name, EntityType.Player, 100)
