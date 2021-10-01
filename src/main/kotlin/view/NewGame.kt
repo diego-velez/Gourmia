9c5ae49fbee69e4c63aec7d904cd1e7e851f9ea1
DVT <DVT12@protonmail.com>
2021-10-01 18:23:34 -0400

    Create a singleton to store objects and created the classes for the entities

--- src/main/kotlin/view/NewGame.kt
+++ src/main/kotlin/view/NewGame.kt
@@ -1,8 +1,15 @@
 package view
 
+import controller.GameRepository
+import model.entities.Player
+
 class NewGame {
     init {
         println("What is your name?")
-        val playerName = readLine()
+        val playerName = readLine() ?: "Player"
+
+        Player(playerName).let { player ->
+            GameRepository.initialize(player)
+        }
     }
 }
